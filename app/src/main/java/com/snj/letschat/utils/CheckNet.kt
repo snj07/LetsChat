bfd9e62f5deaf4589749e8ce119e28f35b14ccdf
snj07 <sanjaysharma4758@gmail.com>
2018-07-22 23:31:36 +0530

    Kotlin code update

---
+++ app/src/main/java/com/snj/letschat/utils/CheckNet.kt
@@ -0,0 +1,12 @@
+package com.snj.letschat.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+class CheckNet{
+    fun isOnline(mContext: Context): Boolean {
+        val cm = mContext
+                .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetworkInfo != null && cm.activeNetworkInfo.isConnectedOrConnecting
+    }
+}
---
+++ app/src/main/java/com/snj/letschat/utils/SharedPrefConfigUtils.kt
@@ -0,0 +1,26 @@
+package com.snj.letschat.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPrefConfigUtils{
+    val MY_PREFERENCES = "MyPrefs"
+    val USER_ID = "uIdKey"
+    val USER_NAME = "uNameKey"
+    val USER_EMAIL = "uEmailKey"
+    val USER_IMAGE = "uImageKey"
+
+    fun getUserId(context: Context): Long {
+        return context.getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE).getLong(USER_ID, 1)
+    }
+    fun getUserEmailId(context: Context): String {
+        return context.getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE).getString(USER_EMAIL, "")
+    }
+    fun getUserName(context: Context): String {
+        return context.getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE).getString(USER_NAME, "")
+    }
+
+    fun getSharedPreference(context: Context): SharedPreferences {
+        return context.getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE)
+    }
+}
